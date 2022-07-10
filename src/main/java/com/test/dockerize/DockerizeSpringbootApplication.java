ed99250f413a6b63796a54262957392d86cd37ff
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-10 10:29:12 +0530

    add simple api

--- src/main/java/com/test/dockerize/DockerizeSpringbootApplication.java
+++ src/main/java/com/test/dockerize/DockerizeSpringbootApplication.java
@@ -2,9 +2,17 @@ package com.test.dockerize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerizeSpringbootApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Springboot Docker Demo";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerizeSpringbootApplication.class, args);
