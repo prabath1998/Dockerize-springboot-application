f46e7a92375b77e73cb0162477d403d5f128b201
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-10 10:25:14 +0530

    Initial commit

---
+++ src/main/java/com/test/dockerize/DockerizeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.test.dockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/dockerize/DockerizeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dockerize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
